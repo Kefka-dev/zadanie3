cc8b7803436fc82903d8023b16a2d88fa46ab7ae
Kefka <50819728+Kefka-dev@users.noreply.github.com>
2023-03-30 11:40:54 +0200

    aplikovana dynamicka alokacia vstupu(funkcna:)

--- main.c
+++ main.c
@@ -5,8 +5,9 @@
 
 #define MAX_LINE_LENGHT 1000
 #define INITIAL_SIZE 16
+#define BODKA '.'
 
-int userInput(int size, char **input);
+int userInput(int size, char ***p_input);
 void printLines(int amount, char **lines);
 
 int main(int argc, char* argv[])
@@ -20,13 +21,27 @@ int main(int argc, char* argv[])
 
     input = (char**)malloc(size * sizeof(char *));
     
-    count = userInput(size, input);
+    count = userInput(size, &input);
 
     while((opt = getopt(argc, argv, optstring)) != -1) {
         switch (opt)
         {
         case 'k':
-            printf("Prepinac -k\n");
+            // int i = 0;
+            // char *section;
+            // section = (char*)malloc(INITIAL_SIZE);
+            // while (optarg[i] != BODKA)
+            // {
+            //     if (i == INITIAL_SIZE)
+            //     {
+            //         size = size +16;
+            //         section = (char*)realloc(section, size);
+            //     }
+                
+            //     section[i] = optarg[i+1];
+            //     i++;
+            // }
+            
             break;
         case 's':
             printf("Prepinac -s a jeho povinny argument %s\n", optarg);
@@ -56,18 +71,28 @@ int main(int argc, char* argv[])
     if (optind == argc)
     {
         printLines(count, input);
+        // for (int i = 0; i < count; i++)
+        // {
+        //     printf("%s", input[i]);
+        // }   
         return 0;
     }
-    
+
+    //uvolnenie naalokovanej pamete
+    // for (int i = 0; i < count; i++)
+    // {
+    //     free(input[i]);
+    // }
+    // free(input);
+
     return 0;
 }
 
-int userInput(int size, char **input)
+int userInput(int size, char ***p_input)
 {
     int count = 0, dvaPrazdne =0, lenght =0;
-
+    char **new_array;
     char buffer[MAX_LINE_LENGHT];
-    
     do
     {
         fgets(buffer, MAX_LINE_LENGHT, stdin);
@@ -75,23 +100,24 @@ int userInput(int size, char **input)
         if (count == size)
         {
             size = size *2;
-            input = (char**)realloc(input, size * sizeof(char*));
+            new_array = (char**)realloc(*p_input, size * sizeof(char*));
+            *p_input = new_array;
         }
         
         lenght = strlen(buffer);
         
         if (buffer[0] == '\n')
         {
-            input[count] = (char*)malloc(2 * sizeof(char));
+            (*p_input)[count] = (char*)malloc(4 * sizeof(char));
             dvaPrazdne++;
         }
         else
         {
             dvaPrazdne = 0;
-            input[count] = (char*)malloc((lenght + 1) * sizeof(char));
+            (*p_input)[count] = (char*)malloc((lenght + 1) * sizeof(char));
         }
         
-        strcpy(input[count], buffer);
+        strcpy((*p_input)[count], buffer);
         count++;
     } while (dvaPrazdne <2);
     return count-2;
