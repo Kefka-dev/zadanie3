d7b9baaf3999a93f941514d1c2bb0d1a80700d76
Kefka <50819728+Kefka-dev@users.noreply.github.com>
2023-03-30 11:40:20 +0200

    do f() uz pasujem pointer na poiner na zaciatok

--- dynamic_memory/dynamic.c
+++ dynamic_memory/dynamic.c
@@ -5,32 +5,33 @@
 #define MAX_LINE_LENGHT 1000
 #define INITIAL_SIZE 16
 
-int userInput(int size, char **input);
+int userInput(int size, char ***p_input);
 
 int main()
 {
     //printf("test");
     int count = 0, size = INITIAL_SIZE;
-    char **input = NULL;
+    char **input;
     input = (char**)malloc(size * sizeof(char *));
     
-    count = userInput(size, input);
+    count = userInput(size, &input);
     
     printf("riadkov %d\n", count);
+    
     for (int i = 0; i < count; i++)
     {
         printf("%s", input[i]);
+        free(input[i]);
     }
-
+    free(input);
     return 0;
 }
 
-int userInput(int size, char **input)
+int userInput(int size, char ***p_input)
 {
     int count = 0, dvaPrazdne =0, lenght =0;
-
+    char **new_array;
     char buffer[MAX_LINE_LENGHT];
-    
     do
     {
         fgets(buffer, MAX_LINE_LENGHT, stdin);
@@ -38,23 +39,24 @@ int userInput(int size, char **input)
         if (count == size)
         {
             size = size *2;
-            input = (char**)realloc(input, size * sizeof(char*));
+            new_array = (char**)realloc(*p_input, size * sizeof(char*));
+            *p_input = new_array;
         }
         
         lenght = strlen(buffer);
         
         if (buffer[0] == '\n')
         {
-            input[count] = (char*)malloc(2 * sizeof(char));
+            (*p_input)[count] = (char*)malloc(4 * sizeof(char));
             dvaPrazdne++;
         }
         else
         {
             dvaPrazdne = 0;
-            input[count] = (char*)malloc((lenght + 1) * sizeof(char));
+            (*p_input)[count] = (char*)malloc((lenght + 1) * sizeof(char));
         }
         
-        strcpy(input[count], buffer);
+        strcpy((*p_input)[count], buffer);
         count++;
     } while (dvaPrazdne <2);
     return count-2;
